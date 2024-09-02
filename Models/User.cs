e1e032baea6b586f3d292464c818bcaa3029d89c
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-02 12:09:28 +0300

    Add a user model

---
+++ Models/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementSystem.Models
+{
+    [Index(nameof(Nickname), IsUnique = true)]
+    [Index(nameof(Email), IsUnique = true)]
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Nickname { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+
+        [Required]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
