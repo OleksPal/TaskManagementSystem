f9ef6ef31dd01cb7cd35126680fa4e36f57e4765
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-02 12:53:56 +0300

    Add Db Context

---
+++ Data/TaskManagementContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagementSystem.Models;
+
+namespace TaskManagementSystem.Data
+{
+    public class TaskManagementContext : DbContext
+    {
+        public TaskManagementContext(DbContextOptions<TaskManagementContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
