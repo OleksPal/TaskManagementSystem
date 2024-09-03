c5bc57ae467b91d819401188af802a66cf651fe2
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-03 10:17:43 +0300

    Implement repository pattern for the UserTask model

--- Repositories/IUserTaskRepository.cs
+++ Repositories/IUserTaskRepository.cs
@@ -5,9 +5,9 @@ namespace TaskManagementSystem.Repositories
     public interface IUserTaskRepository
     {
         Task<ICollection<UserTask>> GetAll();
-        Task<UserTask> GetById(int id);
+        Task<UserTask> GetById(Guid id);
         Task<UserTask> Insert(UserTask task);
         Task<UserTask> Update(UserTask task);
-        Task<UserTask> Delete(int id);
+        Task<UserTask> Delete(Guid id);
     }
 }
