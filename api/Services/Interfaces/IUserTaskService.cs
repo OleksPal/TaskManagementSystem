b36ef80b698469ba16765f03dceb426376315397
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-03 18:21:33 +0300

    Add a UserTask service interface

---
+++ api/Services/Interfaces/IUserTaskService.cs
@@ -0,0 +1,12 @@
+﻿using TaskManagementSystem.Models;
+
+namespace TaskManagementSystem.Services.Interfaces
+{
+    public interface IUserTaskService
+    {
+        Task<UserTask> GetTaskById(Guid id);
+        Task<UserTask> AddTask(Task task);
+        Task<UserTask> EditTask(Task task);
+        Task<UserTask> DeleteTask(Guid id);
+    }
+}
