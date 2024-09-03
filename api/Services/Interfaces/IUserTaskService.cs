af49070e93336a406587c82b8bad914a63f055c2
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-03 18:28:07 +0300

    Implement the UserTask service interface

--- api/Services/Interfaces/IUserTaskService.cs
+++ api/Services/Interfaces/IUserTaskService.cs
@@ -5,8 +5,8 @@ namespace TaskManagementSystem.Services.Interfaces
     public interface IUserTaskService
     {
         Task<UserTask> GetTaskById(Guid id);
-        Task<UserTask> AddTask(Task task);
-        Task<UserTask> EditTask(Task task);
+        Task<UserTask> AddTask(UserTask task);
+        Task<UserTask> EditTask(UserTask task);
         Task<UserTask> DeleteTask(Guid id);
     }
 }
---
+++ api/Services/UserTaskService.cs
@@ -0,0 +1,36 @@
+﻿using TaskManagementSystem.Models;
+using TaskManagementSystem.Repositories.Interfaces;
+using TaskManagementSystem.Services.Interfaces;
+
+namespace TaskManagementSystem.Services
+{
+    public class UserTaskService : IUserTaskService
+    {
+        private readonly IUserTaskRepository _taskRepository;
+
+        public UserTaskService(IUserTaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        public async Task<UserTask> GetTaskById(Guid id)
+        {
+            return await _taskRepository.GetById(id);
+        }
+
+        public async Task<UserTask> AddTask(UserTask task)
+        {
+            return await _taskRepository.Insert(task);
+        }
+
+        public async Task<UserTask> EditTask(UserTask task)
+        {
+            return await _taskRepository.Update(task);
+        }
+
+        public async Task<UserTask> DeleteTask(Guid id)
+        {
+            return await _taskRepository.Delete(id);
+        }
+    }
+}
