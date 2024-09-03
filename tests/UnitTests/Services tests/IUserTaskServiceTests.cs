778b2cf9dd361bde68579b102041066e3d222d68
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-03 18:56:56 +0300

    Add a constructor and helper methods for testing services

---
+++ tests/UnitTests/Services tests/IUserTaskServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using TaskManagementSystem.Models;
+using TaskManagementSystem.Services.Interfaces;
+
+namespace TaskManagementSystem.UnitTests
+{
+    public class UserTaskServiceTests
+    {
+        protected readonly IUserTaskService _userTaskService;
+
+        protected readonly UserTask validTaskObject = new UserTask
+        {
+            Id = Guid.NewGuid(),
+            Title = "Test",
+            Status = Status.Completed,
+            Priority = Priority.Low,
+            CreatedAt = DateTime.Now,
+            UpdatedAt = DateTime.Now
+        };
+
+        public UserTaskServiceTests()
+        {
+            _userTaskService = Helper.GetRequiredService<IUserTaskService>()
+                ?? throw new ArgumentNullException(nameof(IUserTaskService));
+        }
+    }
+}
