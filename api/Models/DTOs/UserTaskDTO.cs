368bdcd4e7b0a67b113aed557f30d066af383031
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-04 16:07:08 +0300

    Add a DTO for the UserTask model

---
+++ api/Models/DTOs/UserTaskDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace TaskManagementSystem.Models.DTOs
+{
+    public class UserTaskDTO
+    {
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? DueDate { get; set; }
+        public Status Status { get; set; }
+        public Priority Priority { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Guid? UserId { get; set; }
+        public User? User { get; set; }
+    }
+}
