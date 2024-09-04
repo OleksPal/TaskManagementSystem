70e357326a1654dc0815485340c438eb8a0d672a
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-04 15:04:24 +0300

    Inject the UserTask service in the UserTask controller

--- api/Controllers/UserTaskController.cs
+++ api/Controllers/UserTaskController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystem.Services.Interfaces;
 
 namespace TaskManagementSystem.Controllers
 {
@@ -7,5 +7,19 @@ namespace TaskManagementSystem.Controllers
     [ApiController]
     public class UserTaskController : ControllerBase
     {
+        private readonly IUserTaskService _userTaskService;
+
+        public UserTaskController(IUserTaskService userTaskService)
+        {
+            _userTaskService = userTaskService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllTasks()
+        {
+            var taskList = await _userTaskService.GetAllTasks();
+
+            return Ok(taskList);
+        }
     }
 }
