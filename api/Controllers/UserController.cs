e104aae99cd4f2491799bf04445d414b6b6c8924
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-07 21:00:59 +0300

    Add a User controller

---
+++ api/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystem.Models;
+
+namespace TaskManagementSystem.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UserController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+    }
+}
