a5c15937844f3b3a70aaf479339314a6b4ce0ec1
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-07 21:12:30 +0300

    Seed new Identity roles in the OnModelCreating method

--- api/Data/TaskManagementContext.cs
+++ api/Data/TaskManagementContext.cs
@@ -11,5 +11,25 @@ namespace TaskManagementSystem.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<UserTask> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole<Guid>> roles = new List<IdentityRole<Guid>>
+            {
+                new IdentityRole<Guid>
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN"
+                },
+                new IdentityRole<Guid> {
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+            };
+
+            builder.Entity<IdentityRole<Guid>>().HasData(roles);
+        }
     }
 }
