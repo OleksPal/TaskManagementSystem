d54efa23a9252d655cbd79e61f9ff2ff704da045
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-08 09:45:40 +0300

    Restrict access to UserTask controller actions only for authorized users

--- api/Program.cs
+++ api/Program.cs
@@ -26,7 +26,6 @@ namespace TaskManagementSystem
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
 
             builder.Services.AddSwaggerGen(option =>
             {
