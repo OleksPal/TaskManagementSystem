5c09e21ab939fcb2777b9168beeb0b19a329738a
User <oleksandr.palamarchuk1310@gmail.com>
2024-09-08 13:25:21 +0300

    Add ChangePassword dto

---
+++ api/DTOs/User/ChangePasswordDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManagementSystem.DTOs.User
+{
+    public class ChangePasswordDto
+    {
+        [Required]
+        public string OldPassword { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
