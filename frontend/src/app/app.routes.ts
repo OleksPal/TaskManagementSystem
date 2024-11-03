4971e021197b23ce5f58ee0e08a535adc2651703
User <oleksandr.palamarchuk1310@gmail.com>
2024-11-03 20:08:48 +0200

    Add a navbar component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
