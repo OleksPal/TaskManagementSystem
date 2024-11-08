43fe0f7da1a9b158ac6d48478d8711a56ecd2234
User <oleksandr.palamarchuk1310@gmail.com>
2024-11-08 22:01:14 +0200

    Add signup form

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SignupComponent } from '../signup/signup.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [SignupComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
