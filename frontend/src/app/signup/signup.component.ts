b43cd97793164fa2429023ada2598fac4902a9c8
User <oleksandr.palamarchuk1310@gmail.com>
2024-11-09 20:11:39 +0200

    Add the model binding for the Signup component

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  signupObj = new SignupModel();
 
+  onRegister(){
+    console.log('Registration was successful');
+    console.log(this.signupObj);
+  }
+}
+
+export class SignupModel {
+  username: string;
+  email: string;
+  password: string;
+
+  constructor(){
+    this.username = "";
+    this.email = "";
+    this.password = "";
+  }
 }
