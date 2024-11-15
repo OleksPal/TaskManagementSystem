920ae35a29adc09c8d03c13b1e27bdbad98b8810
User <oleksandr.palamarchuk1310@gmail.com>
2024-11-15 19:42:24 +0200

    Add the login form

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, SignupComponent],
+  imports: [NavbarComponent, SignupComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
